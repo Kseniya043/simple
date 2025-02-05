b440966831602bb204d3ca54165fe226099a9a69
Kseniya043 <vil-kse@ya.ru>
2025-02-05 21:44:01 +0300

    add mobile menu

---
+++ main.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function () {
+	let burger = document.getElementById('show-burger')
+	let elementWithClassBurger = document.querySelector('.header_cap-mobile');
+	let elementWithClassMenu = document.querySelector('.header_info');
+	let elementWithClassCross = document.querySelector('.burger');
+	burger.onclick = function () {
+	if (elementWithClassBurger.classList.contains("aditional-header_cap-bottom")) {
+		elementWithClassBurger.classList.remove("aditional-header_cap-bottom");
+	} else {
+		elementWithClassBurger.classList.add("aditional-header_cap-bottom")
+	}
+
+	if (elementWithClassMenu.classList.contains("aditional-header_cap-menu")) {
+      	elementWithClassMenu.classList.remove("aditional-header_cap-menu");
+        } else {
+            elementWithClassMenu.classList.add("aditional-header_cap-menu")
+        }
+
+	if (elementWithClassCross.classList.contains("burger-cross")) {
+      	elementWithClassCross.classList.remove("burger-cross");
+        } else {
+            elementWithClassCross.classList.add("burger-cross")
+        }
+	}
+	
+	
+
+})
+	
