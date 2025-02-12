82b2cce7147f2007f216ee305ae28dbd9f6541e3
Kseniya043 <vil-kse@ya.ru>
2025-02-12 20:29:44 +0300

    updating variable name

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 document.addEventListener('DOMContentLoaded', function () {
     let burger = document.getElementById('show-burger')
-    let elementButtonHeader = document.querySelector('.header_cap-mobile');
+    let elementNavigationHeader = document.querySelector('.header_cap-mobile');
     let elementMobileMenu = document.querySelector('.header_menu');
     burger.onclick = function () {
-        if (elementButtonHeader.classList.contains("aditional-header_cap-bottom")) {
-            elementButtonHeader.classList.remove("aditional-header_cap-bottom");
+        if (elementNavigationHeader.classList.contains("aditional-header_cap-bottom")) {
+            elementNavigationHeader.classList.remove("aditional-header_cap-bottom");
         } else {
-            elementButtonHeader.classList.add("aditional-header_cap-bottom")
+            elementNavigationHeader.classList.add("aditional-header_cap-bottom")
         }
 
         if (elementMobileMenu.classList.contains("aditional-header_cap-menu")) {
